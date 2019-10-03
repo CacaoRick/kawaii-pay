daee50a5ef2fe993ada28f0859ace4716f11be46
Rick Liao <cacaorick@gmail.com>
2019-10-03 14:43:16 +0800

    Updates

---
+++ precache-manifest.0432919ae0f41ecc4ea8e4207e125867.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6ee58e34d3c6e77d5e97036003bb71",
+    "url": "/kawaii-pay/index.html"
+  },
+  {
+    "revision": "07da2c1be7674fcc113f",
+    "url": "/kawaii-pay/static/js/2.5cf3b3c8.chunk.js"
+  },
+  {
+    "revision": "a66f6c22f725eb47f5bb",
+    "url": "/kawaii-pay/static/js/main.7dd61c2f.chunk.js"
+  },
+  {
+    "revision": "687dd68c60de2babecc7",
+    "url": "/kawaii-pay/static/js/runtime-main.91b2fbb1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bd97778f9021d3fd36226b499f797790.js"
+  "/kawaii-pay/precache-manifest.0432919ae0f41ecc4ea8e4207e125867.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kawaii-pay/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
