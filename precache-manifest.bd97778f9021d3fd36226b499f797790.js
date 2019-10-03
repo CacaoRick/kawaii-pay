00d24064aceaf752c9f51e43162eaadc696f7526
Rick Liao <cacaorick@gmail.com>
2019-10-03 14:18:56 +0800

    Updates

---
+++ precache-manifest.bd97778f9021d3fd36226b499f797790.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1820195d5f087070edd3d74291d237b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07da2c1be7674fcc113f",
+    "url": "/static/js/2.5cf3b3c8.chunk.js"
+  },
+  {
+    "revision": "5eb600b7fc44781a9504",
+    "url": "/static/js/main.740b9b05.chunk.js"
+  },
+  {
+    "revision": "f590d51f9bf3836cc8f4",
+    "url": "/static/js/runtime-main.28206680.js"
+  }
+]);
